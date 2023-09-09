a0ec838a1a8d2d97c9c000bf1df920a7f8a30fbc
RishinKrishna <rishinkrishna07@gmail.com>
2023-09-09 16:32:37 +0530

    login form validation added

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,8 +2,62 @@ import login_img from "../assets/login_img.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import Styles from "../styles/login.module.css";
+import { useState } from "react";
 
 const login = () => {
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loginError, setLoginError] = useState({
+    emailErr: "",
+    passwordErr: "",
+  });
+
+  const handleChange = (e) => {
+    setLoginForm({
+      ...loginForm,
+      [e.target.name]: e.target.value,
+    });
+    console.log(e.target.value);
+
+    let errorKey = `${[e.target.name]}`;
+    setLoginError({
+      ...loginError,
+      [errorKey]: "",
+    });
+  };
+
+  const validateForm = () => {
+    let emailErr = "";
+    let passwordErr = "";
+
+    if (!loginForm.email && loginForm.email == "") {
+      emailErr = "Please enter your email ";
+    }
+
+    console.log(loginForm.password, loginForm.email);
+
+    if (loginForm.password == "" || loginForm.password.length < 6) {
+      passwordErr = "Please enter your password";
+    }
+
+    if (emailErr || passwordErr) {
+      setLoginError({
+        ...loginError,
+        emailErr,
+        passwordErr,
+      });
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className="flex relative w-full h-[100vh] ">
       <div className="relative w-full h-[20%] md:h-[100vh] hidden md:block">
@@ -18,47 +72,58 @@ const login = () => {
         className={`${Styles[""]} flex w-full md:w-[80%] h-[100%] bg-[#fff] justify-center items-center `}
       >
         <div className="w-full lg:px-[50px]">
-          <div className={`${Styles["form-box"]} flex flex-col justify-center items-center m-auto`}>
-            <h1 className="text-[30px] font-bold text-[#ff6b07] mb-10">
+          <div
+            className={`${Styles["form-box"]} flex flex-col justify-center items-center m-auto`}
+          >
+            <h1 className="text-[30px] font-bold text-[#ff6b07] mb-5">
               TempleAddress
             </h1>
-            <h2 className="text-[30px] font-bold text-[#000] mb-10 ">Login</h2>
+            <h2 className="text-[30px] font-bold text-[#000] mb-4 ">Login</h2>
           </div>
-          <form action="" className="px-5" >
+          <form action="" className="px-5">
             <div className="mb-5 font-semibold">
               <span>Email</span>
               <input
-                type="text"
-                className="w-full py-3 pl-4 outline-none border-[#000] text-[#000] rounded-[6px] mt-2"
+                type="email"
+                name="email"
+                id="email"
+                className="w-full py-3 pl-4 outline-none border border-[#00000052] text-[#000] rounded-[6px] mt-2"
                 placeholder="Enter your email"
+                required
+                onChange={handleChange}
               />
+              <span className="text-[red]">{loginError.emailErr}</span>
             </div>
             <div className="font-semibold">
               <span>Password</span>
               <input
                 type="password"
-                className="w-full py-3 pl-4 outline-none border-[#000] text-[#000] rounded-[6px] mt-2"
+                name="password"
+                id="password"
+                className="w-full py-3 pl-4 outline-none border border-[#00000052] text-[#000] rounded-[6px] mt-2"
                 placeholder=" Enter your password"
+                required
+                onChange={handleChange}
               />
+              <span className="text-[red]">{loginError.passwordErr}</span>
             </div>
             <div className="mt-5 font-semibold">
               <label htmlFor="">
-                <input type="checkbox" />
-                {" "}Remember me
+                <input type="checkbox" /> Remember me
               </label>
             </div>
             <div className="mt-8 flex justify-center items-center">
-              <Link
-                href="/login"
+              <button
+                type="submit"
                 className="py-[9px] font-semibold text-[#fff] px-[150px] bg-[#ff6b07] rounded-[10px]"
+                onClick={handleSubmit}
               >
                 Login
-              </Link>
-              
+              </button>
             </div>
             <div className="mt-4 text-center text-blue-500 font-semibold underline">
-                  <Link href="/signup">Create an Account?</Link>
-                </div>
+              <Link href="/signup">Create an Account?</Link>
+            </div>
           </form>
         </div>
       </div>
