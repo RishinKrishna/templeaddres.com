acee786162d5a470621867b8642e2f9419d1dd29
sajith p j <sajith@vass.co.in>
2023-09-10 15:17:30 +0530

    upadted next js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Outfit } from "next/font/google";
+const outfit = Outfit({
+  subsets: ["latin"],
+  display: "auto",
+  adjustFontFallback: false,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={outfit.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
