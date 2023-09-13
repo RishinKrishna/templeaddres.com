5d7033017e53ebdf082667a79b60d12564ff73a2
sajith p j <sajith@vass.co.in>
2023-09-13 18:30:36 +0530

    added api for searching

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,56 +1,107 @@
-import Styles from '../styles/articles.module.css'
+import Styles from "../styles/articles.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { get } from "@/config/axiosConfig";
+import { debounce, useDebounce } from "@/utils/commonFunction";
+import { useAsyncDebounce } from "react-table";
+import "regenerator-runtime/runtime";
+const Search = ({ setTempelsServices }) => {
+  const [selectedOption, setSelectedOption] = useState("temples");
+  const [query, setQuery] = useState("");
+  const fetchTemples = useAsyncDebounce((value = "") => {
+    get({
+      api: "/temples/list",
+      config: {
+        params: {
+          search_query: value,
+        },
+      },
+    }).then((response) => {
+      setTempelsServices(response.data.data);
+    });
+  }, 500);
 
-const Search = () => {
-  const [selectedOption, setSelectedOption] = useState('Tempels');
+  const fetchServices = useAsyncDebounce((value = "") => {
+    get({
+      api: "/services/list",
+      config: {
+        params: {
+          search_query: value,
+        },
+      },
+    }).then((response) => {
+      setTempelsServices(response.data.data.services);
+    });
+  }, 500);
+
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+    fetchTemples(e.target.value);
+  };
 
   const handleRadioChange = (e) => {
     setSelectedOption(e.target.value);
+
+    if (e.target.value == "temples") {
+      fetchTemples();
+    } else if (e.target.value == "service") {
+      fetchServices();
+    }
   };
 
   return (
-    <div className='w-full h-[45px] my-16 lg:px-[100px] '>
-    <div className= {`flex`}>
-    <input
-  className={`flex relative outline-none border border-[#00000052] h-[43px]  font-semibold rounded-[10px] mr-4 py-7 px-5 w-full h-[45px]  ${Styles["search-input"]}`}
-  type="text"
-  name="searchBox"
-  placeholder="Find Your Temple here..."
-/>
+    <div className="w-full h-[45px] my-16 lg:px-[100px] ">
+      <div className={`flex`}>
+        <input
+          className={`flex relative outline-none border border-[#00000052]  font-outfit  rounded-[10px] mr-4  px-5 w-full   ${Styles["search-input"]}`}
+          type="text"
+          name="searchBox"
+          placeholder="Find Your Temple here..."
+          value={query}
+          onChange={(e) => handleQuery(e)}
+        />
 
-       
-       <FontAwesomeIcon
-              icon={faSearch}
-              className="social_icons mr-3 text-[#fff] rounded-[10px] px-5 py-[15px] bg-[#ff6b07] text-[25px]"
-            />
+        <FontAwesomeIcon
+          icon={faSearch}
+          className="social_icons mr-3 text-[#fff] rounded-[10px] px-5 py-[15px] bg-[#ff6b07] text-[25px]"
+        />
       </div>
       <div className="flex justify-center lg:gap-[200px] my-7 ">
-        <div className="flex text-[18px] font-semibold text-[#666666]">
-        <input
-          type="radio"
-          value='Tempels'
-          checked={selectedOption === 'Tempels'}
-          onChange={handleRadioChange}
-        />
-        <p className=''>Tempels</p>
+        <div className="flex justify-center text-[18px] font-semibold text-[#666666]">
+          <input
+            type="radio"
+            id="temples"
+            value="temples"
+            checked={selectedOption === "temples"}
+            onClick={handleRadioChange}
+          />
+          <label
+            htmlFor="temples"
+            className="font-semibold text-[18px] text-[#666666] ml-2"
+          >
+            Temples
+          </label>
         </div>
-        <div className="flex ">
-        <input
-          type="radio"
-          value='Service'
-          className='ml-5'
-          checked={selectedOption === 'Service'}
-          onChange={handleRadioChange}
-        />
-        <p className='font-semibold text-[18px] text-[#666666]'>Service</p>
+        <div className="flex  justify-center">
+          <input
+            type="radio"
+            id="service"
+            value="service"
+            className="ml-5"
+            checked={selectedOption === "service"}
+            onClick={handleRadioChange}
+          />
+          <label
+            htmlFor="service"
+            className="font-semibold text-[18px] text-[#666666] ml-2"
+          >
+            Service
+          </label>
         </div>
-        
       </div>
-      
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Search
+export default Search;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,33 @@ import { Search, TempleCards, GalaryRow, Events, Articles } from "@/components";
 import circular_pattern_img from "../assets/circular_pattern_img.png";
 import Image from "next/image";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { get } from "@/config/axiosConfig";
 
 const Home = () => {
+  const [tempelsServices, setTempelsServices] = useState([]);
+  const getTemple = () => {
+    get({
+      api: "/temples/list",
+    }).then((response) => {
+      setTempelsServices(response.data.data);
+    });
+  };
+
+  useEffect(() => {
+    getTemple();
+  }, []);
   return (
     <main>
       <Navbar />
       <div className={`flex justify-center items-start sm:px-12 px-6`}>
         <div className={`${styles.boxWidth}`}>
-          <Search />
+          <Search setTempelsServices={setTempelsServices} />
         </div>
       </div>
       <div className={`flex justify-center items-start sm:px-12 px-6`}>
         <div className={`${styles.boxWidth}`}>
-          <TempleCards />
+          <TempleCards serviceCard={tempelsServices} />
         </div>
       </div>
       <div
@@ -28,24 +41,29 @@ const Home = () => {
         </div>
       </div>
       <div className="relative">
-      <div className={`flex justify-center items-start sm:px-12 px-6`}>
-        <div className={`${styles.boxWidth}`}>
-          <Events />
+        <div className={`flex justify-center items-start sm:px-12 px-6`}>
+          <div className={`${styles.boxWidth}`}>
+            <Events />
+          </div>
         </div>
+        <Image
+          alt="sa"
+          src={circular_pattern_img}
+          width={320}
+          height={320}
+          className="left-0 bottom-0 hidden md:block absolute"
+        />
       </div>
-      <Image
-        src={circular_pattern_img}
-        width={320}
-        height={320}
-        className="left-0 bottom-0 hidden md:block absolute"
-      />
-      </div>
-      <div className={`bg-[#fff] flex justify-center items-start sm:px-12 px-6`}>
+      <div
+        className={`bg-[#fff] flex justify-center items-start sm:px-12 px-6`}
+      >
         <div className={`${styles.boxWidth}`}>
           <Articles />
         </div>
       </div>
-      <div className={`bg-[#270d0d] flex justify-center items-start sm:px-12 px-6`}>
+      <div
+        className={`bg-[#270d0d] flex justify-center items-start sm:px-12 px-6`}
+      >
         <div className={`${styles.boxWidth}`}>
           <Footer />
         </div>
